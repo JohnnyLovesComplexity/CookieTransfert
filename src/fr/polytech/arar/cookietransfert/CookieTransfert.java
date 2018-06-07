472cb49d67b991c6236e4772b2962e1baace014c
LeaChemoul <34442368+LeaChemoul@users.noreply.github.com>
2018-06-07 08:27:49 +0200

    op code

--- src/fr/polytech/arar/cookietransfert/CookieTransfert.java
+++ src/fr/polytech/arar/cookietransfert/CookieTransfert.java
@@ -5,6 +5,13 @@ import javafx.stage.Stage;
 
 public class CookieTransfert extends Application {
 
+    private static final byte OP_RRQ = 1;
+    private static final byte OP_WRQ = 2;
+    private static final byte OP_DATA = 3;
+    private static final byte OP_ACK = 4;
+    private static final byte OP_ERROR = 5;
+
+    private final static int PACKET_SIZE = 516; //512 + 2 for the op_code + 2 for the number of the bloc
 
     @Override
     public void start(Stage primaryStage) throws Exception {
