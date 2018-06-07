7cc5f4c8db28e4cd4ef44997bc0601ce05f271c7
LeaChemoul <34442368+LeaChemoul@users.noreply.github.com>
2018-06-07 08:41:58 +0200

    Connexion serveur

--- src/fr/polytech/arar/cookietransfert/CookieTransfert.java
+++ src/fr/polytech/arar/cookietransfert/CookieTransfert.java
@@ -3,6 +3,11 @@ package fr.polytech.arar.cookietransfert;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.ByteArrayOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class CookieTransfert extends Application {
 
     private static final byte OP_RRQ = 1;
@@ -15,7 +20,50 @@ public class CookieTransfert extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        String file = "cookie.txt";
 
+        // STEP 1 : connexion to server
+        InetAddress inetAddress = InetAddress.getByName(ConnectionData.serverIP);
+        DatagramSocket datagramSocket = new DatagramSocket();
+        byte[] requestByteArray = createRequest(OP_RRQ, file, "octet");
+        DatagramPacket datagramPacket = new DatagramPacket(requestByteArray,
+                requestByteArray.length, inetAddress, ConnectionData.serverPort);
+
+        //connection
+
+        // STEP 2: sending request RRQ to pumpkin
+        datagramSocket.send(datagramPacket);
+
+        /*// STEP 3: receive file
+         cr_rv = receiveFile();
+
+        // STEP 4: write file to local disc
+        writeFile(byteOutOS, fileName);*/
+    }
+
+    private byte[] createRequest(final byte op_code, final String file,
+                                 final String mode) {
+        byte zeroByte = 0;
+        int rrqByteLength = 2 + file.length() + 1 + mode.length() + 1;
+        byte[] rrqByteArray = new byte[rrqByteLength];
+
+        int position = 0;
+        rrqByteArray[position] = zeroByte;
+        position++;
+        rrqByteArray[position] = op_code;
+        position++;
+        for (int i = 0; i < file.length(); i++) {
+            rrqByteArray[position] = (byte) file.charAt(i);
+            position++;
+        }
+        rrqByteArray[position] = zeroByte;
+        position++;
+        for (int i = 0; i < mode.length(); i++) {
+            rrqByteArray[position] = (byte) mode.charAt(i);
+            position++;
+        }
+        rrqByteArray[position] = zeroByte;
+        return rrqByteArray;
     }
 
     public static void main(String[] args){
