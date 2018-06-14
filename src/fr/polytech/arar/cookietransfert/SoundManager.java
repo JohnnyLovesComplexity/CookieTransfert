3fd75dff739986751070131d08a6087826f248df
Valentin Berger <Valentin.berger38@gmail.com>
2018-06-14 08:19:55 +0200

    Added system to stop sound

--- src/fr/polytech/arar/cookietransfert/SoundManager.java
+++ src/fr/polytech/arar/cookietransfert/SoundManager.java
@@ -8,14 +8,28 @@ public class SoundManager {
 	
 	public static final String BELL = "bell.wav";
 	
+	private static boolean soundActivated = true;
+	
 	public static void play(String resource) {
-		try {
-			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(SoundManager.class.getResourceAsStream("/sounds/" + resource));
-			Clip clip = AudioSystem.getClip();
-			clip.open(audioInputStream);
-			clip.start();
-		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-			e.printStackTrace();
+		if (isSoundActivated()) {
+			try {
+				AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(SoundManager.class.getResourceAsStream("/sounds/" + resource));
+				Clip clip = AudioSystem.getClip();
+				clip.open(audioInputStream);
+				clip.start();
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+				e.printStackTrace();
+			}
 		}
+	}
+	
+	/* GETTER & SETTER */
+	
+	public static boolean isSoundActivated() {
+		return soundActivated;
+	}
+	
+	public static void setSoundActivated(boolean soundActivated) {
+		SoundManager.soundActivated = soundActivated;
 	}
 }
